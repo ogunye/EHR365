5cd6b6bf938a60634f0986270f742e2fb509a1df
Olufikunmi Ogunye <ogunye_kunmi@yahoo.com>
2023-08-19 06:29:25 +0100

    Project Setup
    Main Project setup with Logger Service setup

---
+++ EHR365.LoggerServices/LoggerManager.cs
@@ -0,0 +1,13 @@
+﻿using EHR365.Application.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHR365.LoggerServices
+{
+    internal class LoggerManager : ILoggerManager
+    {
+    }
+}
---
+++ EHR365.WebApi/Program.cs
@@ -0,0 +1,42 @@
+using EHR365.WebApi.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
